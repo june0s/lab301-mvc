7ee28aec58044db6651d314aa17debfd5719a72c
june0o <cucucy@naver.com>
2025-11-04 14:54:24 +0900

    add json mock tester

---
+++ test-server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+const bodyParser = require('body-parser')
+
+const HTTP_PORT = 3000;
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/users', function(req, res, next) {
+  console.log(`recv ${req.method} ${req.url} port = ${req.secure ? HTTPS_PORT : HTTP_PORT}`)
+  const data = JSON.parse(fs.readFileSync("./data/users-get.json", "utf8"));
+  res.json(data);
+})
+
+app.post('/users', function(req, res, next) {
+  console.log(`recv ${req.method} ${req.url} port = ${req.secure ? HTTPS_PORT : HTTP_PORT}`)
+  console.log(`body ${JSON.stringify(req.body)}`)
+  const data = JSON.parse(fs.readFileSync("./data/users-post.json", "utf8"));
+  res.send(data);
+})
+
+http.createServer(app).listen(HTTP_PORT);
+
+console.log(`server started... listening port is ${HTTP_PORT}`)
